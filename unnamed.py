be827f61a216fee91dd356a3f911f974a37c0bb9
Gaham <glebsemenov1mail.ru@gmail.com>
2025-07-14 22:11:39 +0300

    Create unnamed.py

---
+++ unnamed.py
@@ -0,0 +1,19 @@
+stack=[];var={}
+out="";code="""
+
+"""
+s=0;tr=0;p=0;code=code[1:len(code)-1]
+if code=="":s=1;out+=input()
+while len(code)>p and s==0:
+ if len(out)>1023:tr=1;break
+ if code[p]in list(map(str,range(10))):stack.append(int(code[p]))
+ if code[p]=="•":var[code[p+1]]=stack.pop()
+ if code[p]==",":
+  try:
+   if code[p+1] in var:stack.append(var[code[p+1]])
+   else:out+=str(stack.pop())
+  except:out+=str(stack.pop())
+ p+=1
+print("~"*15+" RESULT "+"~"*15)
+print(out,"~"*38,"STACK: "+str(stack),"VARIABLES: "+str(var),sep="\n")
+if tr:print("T")
