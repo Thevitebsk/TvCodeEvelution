0d454d200dcc7ef8a28369badf3804f9a38921f6
Gaham <glebsemenov1mail.ru@gmail.com>
2025-07-14 22:17:13 +0300

    Update old.py

--- 4ME/old.py
+++ 4ME/old.py
@@ -1,17 +1,18 @@
 #3Mwh.py
 
 #important stuff
-print("4ME!")
-from tkinter.filedialog import askopenfilename;import re
+print("4me. The language created by Gaham")
+from tkinter.filedialog import askopenfilename
+import re
 def imp():
     pa=askopenfilename(filetypes=[("Work-Machine Files", "*.wm")])
     if pa:
-        with open(pa,'r') as file:lines=[line.rstrip() for line in file]
+        with open(pa, 'r') as file:lines=[line.rstrip() for line in file]
         return lines
 
 # main code loop
 while True:
-    error=c=0; i=imp(); ln=1; s=[]; ss=[]; ts=[]
+    error=False; inpu=""; i=imp(); ln=1; s=[]; ss=[]; ts=[]; v=""; c=0
     if i != None:
         for line in i:
             ln+=1
@@ -21,7 +22,7 @@ while True:
                 for match in matches: print(match)
                 if line=="is": s.append(input("string:"))
                 if line=="ns": s.append(int(input("number:")))
-                if line=="": print(f"S1456\nCAN YOU. I DON'T KNOW. USE COMMANDS IN THE LINE\nERROR AT LINE {ln}");error=1
+                if line=="": print(f"S1456\nCAN YOU. I DON'T KNOW. USE COMMANDS IN THE LINE\nERROR AT LINE {ln}");error = True
                 matches=re.findall(r'add{(.*?)}',line)
                 for match in matches: s.append(match)
                 if line=="remove": s.pop(0)
@@ -30,9 +31,9 @@ while True:
                 if line=="-":s.append(int(s.pop(0))-int(s.pop(0)))
                 if line=="*":s.append(int(s.pop(0))*int(s.pop(0)))
                 if line=="/":s.append(int(s.pop(0))//int(s.pop(0)))
-                if line=="duplicate": s.append(s[0])
-                if line=="store": ss.append(s.pop(0))
-                if line=="unstore": s.append(ss.pop(0))
-                if line=="E;": break
-            else: print("S1245\nP: IS MISSING\nERROR AT LINE 1");error=1
-            if error==1: break
+                if line == "duplicate": s.append(s[0])
+                if line == "store": ss.append(s.pop(0))
+                if line == "unstore": s.append(ss.pop(0))
+                if line == "E;": break
+            else: print("S1245\nP: IS MISSING\nERROR AT LINE 1");error=True
+            if error == True: break
