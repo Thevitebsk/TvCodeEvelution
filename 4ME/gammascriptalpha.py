c689fb18c31b0b5b73f66df775d2f862e7c2df8f
Gaham <glebsemenov1mail.ru@gmail.com>
2025-07-14 12:35:07 +0300

    Create gammascriptalpha.py

---
+++ 4ME/gammascriptalpha.py
@@ -0,0 +1,22 @@
+#MPUG.py
+#this version of gammascript is really incomplete and will probably never will be complete
+import re
+class Lexer(object):
+    def __init__(self, source_code):
+        self.source_code=source_code
+    def tokenize(self):
+        token=[]
+        source_code=self.split()
+        source_index=0
+        while source_index<len(source_code):
+            word = source_code[source_index]
+            if word == "write":
+                token.append(["WRITE", word])
+            elif re.match('[a-z]', word) or re.match('[A-Z]', word):
+                token.append(['IDENTIFIER', word])
+            elif re.match('[0-9]', word):
+                token.append(['IDENTEGER', word])
+            source_index +=1
+        print(token)
+        return token
+Lexer.tokenize("writetolog 12")
