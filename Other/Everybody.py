7b4327c9f68ce45b779fbe34f1119a4c1c2eb679
Gaham <glebsemenov1mail.ru@gmail.com>
2025-08-06 22:53:07 +0300

    added more features

--- Other/Everybody.py
+++ Other/Everybody.py
@@ -1,4 +1,5 @@
 # Everybody! - langauge inspred by "@everyone" (see https://esolangs.org/wiki/@everyone for more info about "@everyone")
+
 import sys
 print("Everybody!")
 variables={}
@@ -8,6 +9,18 @@ except IndexError:print("no file was used"); exit()
 p=0
 while p!=len(code): code[p]=code[p].replace("\n",""); p+=1
 
+def evaluate(arg) -> bool:
+    a=arg[0][0:-1]
+    p=0
+    
+    ayl=a.replace("->"," -> ").split(" ")
+    print(a,ayl)
+
+    if len(ayl)==1 and ayl[0][0]=="@":
+        return bool(variables[ayl[0][1::]])
+    elif len(ayl)==1 and ayl[0][0]!="@":
+        return bool(ayl[0][1::])
+
 p=0
 try:
     while code[p]!="Hello everyone!": p+=1
@@ -15,7 +28,15 @@ except IndexError:
     raise Exception("\nERROR INFO:\n"+
                    "No starting point found")
 while p!=len(code):
-    if code[p][0]=="@"and code[p].split(" ")[0][-1]==","and" ".join(code[p].split(" ")[1:])=="at least say something":
-        variables[code[p].split(" ")[0][1:-1]]=input()
-        print(variables)
+    c=code[p].strip().split(" ")
+    print(c)
+    if c[0][0]=="@"and c[0][-1]==","and" ".join(c[1:])=="at least say something":
+        variables[c[0][1:-1]]=input()
+    if " ".join(c[0:2])=="Quick question,"and" ".join(c[-3:len(c)])=="If so, then:":
+        if evaluate(c[2:-3][0:len(c)-2]): ...
+        else:
+            while c!="" and p!=len(code):
+                c=code[p].strip()
+                p+=1
+            p-=1
     p+=1
