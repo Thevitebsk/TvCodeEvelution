757f043df96c47da91d4e186f3d624b4cb901c4a
Gaham <glebsemenov1mail.ru@gmail.com>
2025-08-23 13:01:02 +0300

    Update oldunai.py

--- Other/oldunai.py
+++ Other/oldunai.py
@@ -1,24 +1,24 @@
-#okXk.py
+#Hell.py
 
-# --UNAI v1.0-- #
-stack=[];tempstack=[];point=force=0;code=input()
+# --UNAI-- #
+s=[];ts=[];p=io=0;c=input()
 def push_int():
-    global point
+    global p
     while 1:
-     if point+1>len(code):break
-     tempstack.append(code[point]);point+=1
-     if code[point]not in list(map(str,range(10))):point-=1;break
-    stack.append("".join(tempstack));tempstack.clear()
-while len(code)>point:
-    if code[point]in list(map(str,range(10))):push_int()
-    elif code[point]==".":print(stack.pop());force=1
-    elif code[point]=="\"":
-        point+=1
-        while code[point]!="\"":tempstack.append(code[point]);point+=1
-        stack.append("".join(tempstack));tempstack.clear()
-    elif code[point]=="#":stack.pop()
-    elif code[point]=="$":stack.append(stack.pop(len(stack)-2))
-    elif code[point]==",":stack.append(input())
-    elif code[point]=="[":point=code.index("]",point)
-    point+=1
-if force==0:print("\n".join(stack))
+     if p+1>len(c):break
+     ts.append(c[p]);p+=1
+     if c[p]not in"0123456789":p-=1;break
+    while ts[0]=="0":ts.pop(0)
+    s.append("".join(ts));ts.clear()
+while len(c)>p:
+    if c[p]in"0123456789":push_int()
+    elif c[p]==".":print(s.pop());io=1
+    elif c[p]=="\"":
+        p+=1
+        while c[p]!="\"":ts.append(c[p]);p+=1
+        s.append("".join(ts));ts.clear()
+    elif c[p]=="`":s.pop()
+    elif c[p]==",":s.append(input())
+    elif c[p]=="[":p=c.index("]",p)
+    p+=1
+if io==0:print("\n".join(s))
