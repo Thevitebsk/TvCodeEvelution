0f2f97fac711371304cd8ae0a730960159a329f9
Gaham <glebsemenov1mail.ru@gmail.com>
2025-08-23 17:21:43 +0300

    Update oldestgammascriptimplementationever.py

--- 4ME/oldestgammascriptimplementationever.py
+++ 4ME/oldestgammascriptimplementationever.py
@@ -1,9 +1,25 @@
-#RnmR.py + 5lMx.py
-def main():
-    content = ""
-    with open('test.ger') as file:
-        content = file.read()
-main()
+#U8LL.py + T70I.py
+import re
 class Lexer(object):
     def __init__(self, source_code):
         self.source_code=source_code
+    def tokenize(self):
+        token=[]
+        source_code=self.source_code.split()
+        source_index=0
+        while source_index<len(source_code):
+            word = source_code[source_index]
+            if word == "writetolog":
+                token.append(["WRITETOLOG_DECLARATION", word])
+            elif re.match('[a-z]', word) or re.match('[A-Z]', word):
+                token.append(['IDENTIFIER', word])
+            source_index +=1
+        print(token)
+        return token
+def main():
+    content = ""
+    with open('test.gr') as file:
+        content = file.read()
+    lex=Lexer(content)
+    tokens=lex.tokenize()
+main()
