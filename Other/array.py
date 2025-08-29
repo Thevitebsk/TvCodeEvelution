6525a0742903f2214d67c8292a0446bd3d02cc72
Gaham <glebsemenov1mail.ru@gmail.com>
2025-08-29 23:01:26 +0300

    Update array.py

--- Other/array.py
+++ Other/array.py
@@ -1,7 +1,7 @@
 #xamj.py
 
 print("Array?")
-c="""[Hello\, world!]""";p=m=0;s=[];ar=[];i=''
+c="""[Hello\\, world!]""";p=m=0;s=[];ar=[];i=''
 while p<len(c):
  if c[p]=="[" and m==0:m=1
  elif m==1:
@@ -16,7 +16,7 @@ while p<len(c):
    s.reverse()
    while len(s)>1:s.append(s.pop()+s.pop())
    ar.append(s.pop(0).replace("\\",""))
- elif c[p]=="∃": p=c.index("∄")
+ elif c[p]=="∃": p=c.index("∄",p)
  elif c[p]=="□": ar.append(input())
  p+=1
 print("\n".join(ar))
