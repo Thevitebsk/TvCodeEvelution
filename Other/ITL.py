ec26e2cec4b6d2c64464eb2657069771ffa903bc
Gaham <glebsemenov1mail.ru@gmail.com>
2025-08-29 12:00:48 +0300

    Update ITL.py

--- Other/ITL.py
+++ Other/ITL.py
@@ -20,24 +20,51 @@ try:
 except IndexError:...
 
 # The actual implementation:
-class MusuM():
+class ITL():
     def lex(self,x:str):
         #Make every command as an entry to the mapper 
         global p,n
-        o="";keys="f";ts="()<>,:";mapper={}
+        o="";keys={
+            0  :[":"],    #Non-argumented keywords
+            1  :["f"],    #One-argumented keywords
+            "b":["give"], #Keywords that take a bracket (such as parenthesis) as an argument
+            };ts="()<>,:";mapper={}
         [o:=o+i if i not in ts else o+f" {i} "for i in x];y=o.strip().split()
         while p<len(y):
-            if y[p]=="<":
-                y[p:y.index(">",p)+1]=""
-                if len(y)!=0: p=-1;n=0
-                else: break
-            elif y[p]in[""]:y[p]=""
-            elif y[p]in[ts,keys]:
-                try:mapper[f"{y[p]}"][0].append(n)
-                except KeyError:mapper[f"{y[p]}"]=[[n],"KEYWORD"]
+            #Get rid of comments and empty elements in "y"
+            if y[p]!="(":
+                if y[p]=="<":
+                    y[p:y.index(">",p)+1]=""
+                    if len(y)!=0: p=-1
+                    else: break
+                elif y[p]in[""]:y[p]=""
+            else:...
+            p+=1
+
+        p=0
+        while p<len(y):
+            #Check type of keyword
+            o=""
+            if y[p]in keys[0]:
+                try:mapper[y[p]][0].append(n)
+                except KeyError:mapper[y[p]]=[[n],"KEYWORD"]
+            elif y[p]in keys[1]:
+                try:
+                    mapper[y[p]+" "+y[p+1]][0].append(n)
+                    p+=1;y[p]=""
+                except KeyError:
+                    mapper[y[p]+" "+y[p+1]]=[[n],"KEYWORD"]
+                    p+=1;y[p]=""
+            elif y[p]in keys["b"]:
+                try:
+                    mapper[y[p]][0].append(n)
+                except KeyError:
+                    [o:=o+i+" "if i in"(),"else o+i for i in y[p+2:y.index(")",p)]]
+                    mapper[y[p]+y[p+1]+o+y[y.index(")",p)]]=[[n],"COMMAND"]
+                    p=y.index(")",p);y[p:y.index(")",p)]=""
             else:
-                try:mapper[f"{y[p]}"][0].append(n)
-                except KeyError:mapper[f"{y[p]}"]=[[n],"ID"]
+                try:mapper[y[p]][0].append(n)
+                except KeyError:mapper[y[p]]=[[n],"ID"]
             n+=1;p+=1
         if debug:print("Command map:",mapper)
 
@@ -58,4 +85,4 @@ print("""
 @@@   @@@   @@@@@@
 @@@   @@@   @@@@@@
 """[1:-1])
-MusuM(c)
+ITL(c)
