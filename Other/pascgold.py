b91ce815b50a9680fc3990fd49440d7ec64a2ea0
Gaham <glebsemenov1mail.ru@gmail.com>
2025-08-30 12:06:31 +0300

    Update pascgold.py

--- Other/pascgold.py
+++ Other/pascgold.py
@@ -2,25 +2,20 @@
 
 print("Pascgolf")
 i=input("Input your code:\n");p=0
-o=0;e=0;v={"n":[],"v":[]};ts=[]
+o=0;v={};ts=[]
 try:
    while len(i)>p:
-      if e==1:break
-      elif i[p]=="[":o=1
-      elif i[p]=="]":o=0
-      elif i[p+1]==":"and i[p]=="v":
-         v["n"].append(i[p+2]);p+=3
-         while i[p]!=";":ts.append(i[p]);p+=1
-         ts.reverse()
-         while len(ts)>1:ts.append(ts.pop()+ts.pop())
-         v["v"].append(ts.pop())
+      if i[p]=="[": o+=1
+      elif i[p]=="]": o-=1
+      elif i[p]=="v": v[i[p+1]]=i[p+2:i.index(";",p)];p=i.index(";",p)
       elif i[p]==".":
          if i[p+1]=="\"":
+            print(i[p+2:i.index("\"",p+2)]);p=i.index("\"",p+2)
+         if i[p+1]=="v":
+            try:print(v[i[p+2]])
+            except KeyError: print(f"No varible named \"{i[p+2]}\" exists")
             p+=2
-            while i[p]!="\"":print(i[p],end="");p+=1
-         if i[p+1]=="v"and i[p+2]==":":
-            p+=3;ind=v["n"].index(i[p]);print(v["v"][ind])
-      elif i[p]!="["or"]"and o==0:e=1;print("command not in brackets")
-      else:e=1;print("not a valid pascgolf command",i[p])
+      elif i[p]not in"[]"and o==0:print("command not in brackets");exit()
+      else:print(f"not a valid pascgolf command \"{i[p]}\" -> {p}");exit()
       p+=1
 except IndexError:...
