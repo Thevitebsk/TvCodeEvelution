512b6b4a47eced5966b38d59b2acaff5b043fa84
Gaham <glebsemenov1mail.ru@gmail.com>
2025-08-30 11:22:17 +0300

    Update p1.py

--- Other/p1.py
+++ Other/p1.py
@@ -1,10 +1,10 @@
 #-! p1 !-#
 #a langauge made in python 1
-c="""qHello, worldp""";p=0;s=[];ts=[];out=1
+c="""qHello, worldp""";p=0;s=[];ts=[]
 while p<len(c):
  if c[p]=="#":
   c.index("#",p+1)
  if c[p]=="q":
   s.append(c[p+1:c.index("p",p+1)])
  p=p+1
-if s and out:print("".join(s))
+if s:print("".join(s))
