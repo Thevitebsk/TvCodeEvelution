1eeed3726d59fa1922cbcbe9c037ca63ac9f1a10
Gaham <glebsemenov1mail.ru@gmail.com>
2025-08-31 10:13:02 +0300

    Update and rename pascgold.py to pascgolf.py

--- Other/pascgolf.py
+++ Other/pascgolf.py
@@ -1,21 +1,24 @@
 #IeLe.py (snippet)
 
 print("Pascgolf")
-i=input("Input your code:\n");p=0
-o=0;v={};ts=[]
+i=input("Input your code:\n");p=o=0
+v={};out=""
 try:
-   while len(i)>p:
+   while 1:
+      if i[p]not in"["and o==0:print("command not in brackets");exit()
+      elif i[p]=="]"and p==len(i)-1: o=-1+1
       if i[p]=="[": o+=1
       elif i[p]=="]": o-=1
       elif i[p]=="v": v[i[p+1]]=i[p+2:i.index(";",p)];p=i.index(";",p)
       elif i[p]==".":
          if i[p+1]=="\"":
-            print(i[p+2:i.index("\"",p+2)]);p=i.index("\"",p+2)
+            out+=i[p+2:i.index("\"",p+2)]+" ";p=i.index("\"",p+2)
          if i[p+1]=="v":
-            try:print(v[i[p+2]])
+            try:out+=v[i[p+2]]+" "
             except KeyError: print(f"No varible named \"{i[p+2]}\" exists")
             p+=2
-      elif i[p]not in"[]"and o==0:print("command not in brackets");exit()
       else:print(f"not a valid pascgolf command \"{i[p]}\" -> {p}");exit()
+      if o==-1:break
       p+=1
-except IndexError:...
+except IndexError:print("Program did not encounter an end statement!");exit()
+print(out)
