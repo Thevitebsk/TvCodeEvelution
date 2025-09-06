0090a9ab770107d0daefd048f90384865b993046
Gaham <glebsemenov1mail.ru@gmail.com>
2025-09-06 23:22:24 +0300

    tried to fix the string problem

--- Other/ITL.py
+++ Other/ITL.py
@@ -81,8 +81,8 @@ class Interactive_Thon_Langauge():
                 for i in z:
                     try:
                         if i[0]=="\"":
-                            print(i[1:-1],end=" ");m=1
-                            if i[0]!="\""and m:print(i[1:-1],end=" ")
+                            print(i.replace("\"","")],end=" ");m=1
+                            if i[0]!="\""and m:print(i.replace("\"",""),end=" ")
                             elif i[0]=="\""and m:print();m=0
                         elif is_int(i):print(i)
                         else:print("...")
