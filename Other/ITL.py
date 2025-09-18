c9c122af6134a9d7d89d7dfe1b378fb3b25333b8
Gaham <glebsemenov1mail.ru@gmail.com>
2025-09-18 23:44:56 +0300

    fixed some stuff ig

--- Other/ITL.py
+++ Other/ITL.py
@@ -1,13 +1,23 @@
 from sys import argv
-p=n=f=debug=0;a=1
+p=f=debug=0;n=a=1
 try:c=open(argv[1]).read()
 except IndexError:f=1;c=""
 except FileNotFoundError:
-    if argv[1][0]!="-":print("ITL: The file selected doesn't exist\n");exit()
+    if argv[1][0]!="-":print("ITL: The file selected doesn't exist");exit()
     else:c="";f=1
 else:a=2
 try:
-    if argv[a][0]=="-":
+    if argv[a]=="-h":
+        print("ITL for begineers!",
+              "To make a program using the REPL. "
+              "\nMake sure to not include a file name, "
+              "else the code from the file will be executed",
+              "Once your at the REPL, \nlocate the \"1: \" text, "
+              "this is where you put your line of code (e.g. give(\"hi!\"))",
+              "And to execute your program in the REPL,\n"
+              "you just input \"end\" to the code line input"
+              ,sep="\n\n");exit()
+    elif argv[a][0]=="-":
         a=argv[a:]
         while 1:
             try:a
@@ -75,14 +85,14 @@ class Interactive_Thon_Langauge():
         funcs={};p=0
         while p!=len(x):
             y=x[p].split(" ")
-            if y[0]=="f"and x[p+1][0]==":":funcs[y[1]]=[p,x[p+3].split(",")];p+=1
+            if y[0]=="f"and x[p+1][0]==":":funcs[y[1]]=p;p+=1
             elif y[0]=="give"and y[1]=="(":
                 z=" ".join(y[2:y.index(")")]).split(">")
                 for i in z:
                     try:
                         if i[0]=="\"":
-                            print(i.replace("\"",""),end=" ");m=1
-                            if i[0]!="\""and m:print(i.replace("\"",""),end=" ")
+                            print(i[1:-1],end=" ");m=1
+                            if i[0]!="\""and m:print(i[1:-1],end=" ")
                             elif i[0]=="\""and m:print();m=0
                         elif is_int(i):print(i)
                         else:print("...")
@@ -92,19 +102,19 @@ class Interactive_Thon_Langauge():
 
     def __init__(self,code): self.run(self.lex(code))
 print("""
-@@@@@@@@@@@@@@@
-@@@@@@@@@@@@@@@
- @    @@@   @@@
-      @@@   @@@
- @    @@@   @@@
-@@@   @@@   @@@
-@@@   @@@   @@@
-@@@   @@@   @@@
-@@@   @@@   @@@
-@@@   @@@   @@@@@@
-@@@   @@@   @@@@@@
+@@@@########!!!
+@@@@########!!!
+ @    ###   !!!
+      ###   !!!
+ @    ###   !!!
+@@@   ###   !!!
+@@@   ###   !!!
+@@@   ###   !!!
+@@@   ###   !!!
+@@@   ###   !!!!!!
+@@@   ###   !!!!!!
 """[1:-1])
 if f:
-    while (i:=input("Interactive Thon Langaugeㅣ")):c+=i
+    while (i:=input(f"{n}: "))!="end":c+=i;n+=1
     Interactive_Thon_Langauge(c)
 else:Interactive_Thon_Langauge(c)
