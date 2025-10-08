df0a2e699b73eef68ba663c639ef009e5f87b306
Gaham <glebsemenov1mail.ru@gmail.com>
2025-10-08 22:35:03 +0300

    Update BPIAPBP.py

--- Other/BPIAPBP.py
+++ Other/BPIAPBP.py
@@ -1,9 +1,18 @@
-# BPINPBP Program Isn't A Valid Python Bytecode Program
-
-INST = {"0x0":"HALT","0x1":"PUSH_REFRENCE"}
-opcodes=[];p=debug=0;refs=[]
+# BPINPBC Program Isn't A Valid Python ByteCode Program
+INST = {
+    "0":"NOP",
+    "1":"PUSH",
+    "2":"ADD",
+    "3":"SUB",
+    "4":"MUL",
+    "5":"DIV",
+    "6":"JIC",
+    "7":"LJP",
+    "e":"OUT",
+    "f":"HALT"}
+import sys,time
+opcodes=[];p=debug=0;n=[];tn=[];l={};time1=time.time()
 print("BPINPBC Programs Aren't A Valid Python ByteCode Program")
-import sys
 try:sys.argv[2]
 except IndexError:...
 else:
@@ -11,28 +20,44 @@ else:
     while len(a)>0:
         if a[0] in ["-d","--debug"]: debug=1
         a.pop(0)
-try:code=sys.argv[1]
+try:code=open(sys.argv[1]).read()
 except IndexError:
     raise FileNotFoundError("No file was given")
-try:code=open(code).read()
 except FileNotFoundError:
-    raise FileNotFoundError(f"File {code} doesn't exist")
-[opcodes.append(hex(ord(i)))for i in code];opcodes.append("0x0")
+    raise FileNotFoundError(f"File {sys.argv[1]} doesn't exist")
+for i in code:
+    opcodes.append(hex(ord(i))[2])
+    try:opcodes.append(hex(ord(i))[3])
+    except IndexError:opcodes.append(0)
+while opcodes[-1]=="f":opcodes.pop()
+opcodes.append("f")
+while p!=len(opcodes):exec("l[opcodes[p+1]]=p")if opcodes[p]=="7"else...;p+=1
+p=0;print(opcodes,l)
 while p!=len(opcodes):
+    if time1-time.time()<-30.0:break
     try:INST[opcodes[p]]
     except KeyError:print(f"SYSTEM ERROR: INVALID OP CODE {opcodes[p]}"); break
-    if opcodes[p]=="0x0": break
-    elif opcodes[p]=="0x1":
-        refs.append({f"{opcodes[p+1]}":0}); p+=1
-        opcodes[p]+=" val"
+    if opcodes[p]=="f": break
+    elif opcodes[p]=="1": n.append(opcodes[p+1]); p+=1
+    elif opcodes[p]=="2": n.append(n.pop()+n.pop())
+    elif opcodes[p]=="3": n.append(n.pop()-n.pop())
+    elif opcodes[p]=="4": n.append(n.pop()*n.pop())
+    elif opcodes[p]=="5": n.append(n.pop()/n.pop())
+    elif opcodes[p]=="6":
+        tn.append([n[-1]<n[-2],n[-1]==n[-2],n[-1]>n[-2]])
+        if opcodes[p+1]=="1"and tn[0][0]: p=l.get(p+2,0)
+        elif opcodes[p+1]=="2"and tn[0][1]: p=l.get(p+2,0)
+        elif opcodes[p+1]=="3"and tn[0][2]: p=l.get(p+2,0)
+        tn.clear()
     p+=1
 if debug:
-    print(refs)
-    p=1
+    print(n)
     print("N:      HEX:    OPCODE:")
     for i, opcode in enumerate(opcodes):
-        print(f"{i + 1:<7} {opcode[0:4]:<8}", end="")
+        print(f"{i + 1:<7} {str(opcode)[0]:<8}", end="")
         try:print(INST[opcode])
         except KeyError:
-            print("NOP") if opcode[len(opcode)-3::]!="val" else print(f"{"ARGUMENT":<8}")
+            if opcode[len(opcode)-3::]!="val":print("NOP")
+            else:print(f"{"ARGUMENT":<8}")
         p+=1
+    print("Glyphs:\n"+code)
