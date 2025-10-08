949560c924c40752a1d64c29466e978d74d6e0e8
Gaham <glebsemenov1mail.ru@gmail.com>
2025-10-08 23:04:05 +0300

    Update newcfilorux.py

--- Other/newcfilorux.py
+++ Other/newcfilorux.py
@@ -3,14 +3,14 @@ t=[];codef=[]
 from sys import argv;from time import time
 
 # Input
-stack=[]; point=force=n=0
+stack=[]; point=n=0
 try:codef=open(argv[1]).read()
 except:
   while (code:=input(f"{n+1}: ")).strip()!="":codef.append(code);n+=1
 
 # The Language
-start=time()
-code="\n".join(codef)
+start=time();out=""
+code = "\n".join(codef)
 # print(code)
 def numb()->str:
   global point
@@ -21,6 +21,9 @@ def numb()->str:
 while len(code)>point:
   if code[point]in"0123456789":stack.append(numb())
   elif code[point]==",":t.append(f"{time()-start:.5f}");stack.append(input());start=time()
+  elif code[point]==".":out+=str(stack.pop())
   point+=1
+
 x=time()-start;[x:=x+float(i)for i in t]
-print(stack,f"{x:.5f} seconds")
+print("-"*38+"\n"+out if out else"")
+print("-"*38,f"{x:.5f} seconds",sep="\n")
