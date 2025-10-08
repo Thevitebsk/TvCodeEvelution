fa99f9e342401b0e0211ae82f898158bd66a1f39
Gaham <glebsemenov1mail.ru@gmail.com>
2025-10-08 22:45:12 +0300

    Update newcfilorux.py

--- Other/newcfilorux.py
+++ Other/newcfilorux.py
@@ -1,16 +1,12 @@
 print("CFILORUX\n"+"-"*38)
-codef = []
+t=[];codef=[]
 from sys import argv;from time import time
-# Arguments
-try:codef=open(argv[1]).read()
-except:...
 
 # Input
-stack = []; tempstack = []; point = force = 0; n = 1
-if not codef:
-  while (code:=input(f"{n}: ")).strip()!="":codef.append(code);n+=1
-else: ...
-...if codef else exit()
+stack=[]; point=force=n=0
+try:codef=open(argv[1]).read()
+except:
+  while (code:=input(f"{n+1}: ")).strip()!="":codef.append(code);n+=1
 
 # The Language
 start=time()
@@ -18,13 +14,13 @@ code = "\n".join(codef)
 # print(code)
 def numb()->str:
   global point
-  listnumb=[]
-  while point!=len(code)and code[point]in"0123456789":
-    listnumb.append(code[point]);point+=1
-  return "".join(listnumb)
+  l=[]
+  while point!=len(code)and code[point]in"0123456789":l.append(code[point]);point+=1
+  point-=1;return int("".join(l))
 
 while len(code)>point:
-  if code[point]in"0123456789":stack.append(int(numb()))
-  elif code[point]==",":stack.append(input())
+  if code[point]in"0123456789":stack.append(numb())
+  elif code[point]==",":t.append(f"{time()-start:.5f}");stack.append(input());start=time()
   point+=1
-print(stack,f"{time()-start:.5f} seconds")
+x=time()-start;[x:=x+float(i)for i in t]
+print(stack,f"{x:.5f} seconds")
