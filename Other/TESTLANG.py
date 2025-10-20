d3b6f284824b6f0602e45a32a83345497dc0ad7c
Gaham <glebsemenov1mail.ru@gmail.com>
2025-10-20 12:27:28 +0300

    Update TESTLANG.py

--- Other/TESTLANG.py
+++ Other/TESTLANG.py
@@ -1,29 +1,20 @@
 i=input(">>").split()
-cmd=0;s=[];inp='';o=[]
-while len(i)!=0:
+cmd=0;s=[];o=""
+while 1:
         cmd+=1
         if i[0]=="out":
-            if i[1]=="char":
-                o.append(chr(int(i.pop(1))))
-                i.pop(0);i.pop(0)
-            else:
-                o.append(i.pop(1));i.pop(0)
-        elif c[0]=="note":c.pop(0);c.pop(0)
-        elif c[0]=="in":inp=input("input\n");c.pop(0)
-        elif c[0]=="push":
-            if c[1]=="in":
-                s.append(inp)
-                c.pop(0);c.pop(0)
-            elif c[1]=="out":
-                o.append(s.pop(0))
-                c.pop(0);c.pop(0)
-            else:
-                s.append(c[1])
-                c.pop(0);c.pop(0)
-        elif c[0]=="op":
-            if c[1]=="plus":
-                r=int(s[0])+int(s[1])
-                s.append(int(s.pop(0))+int(s.pop(0)))
-                c.pop(0);c.pop(0)
-        elif c[0]=="end":print("".join(str(o)));break
-        else:print(f"CMD:{cmd} UNDEFINED COMMAND");break
+            if i[1]=="char":  o+=chr(int(i.pop(1)))+" ";i.pop(0)
+            else:  o+=i.pop(1);i.pop(0)
+        elif i[0]=="note":  i.pop(0)
+        elif i[0]=="push":
+            if i[1]=="in":  s.append(input("input\n"));i.pop(0)
+            elif i[1]=="out":  o+=str(s.pop())+" ";i.pop(0)
+            elif i[1]=="top":  s.append(s[-1]);i.pop(0)
+            else:  s.append(i[1]);i.pop(0)
+        elif i[0]=="op":
+            if i[1]=="plus":  s.append(int(s.pop())+int(s.pop()));i.pop(0)
+            elif i[1]=="neg":  s.append(int(s.pop())*-1);i.pop(0)
+            elif i[1]=="div":  s.append(int(s.pop())/int(s.pop()));i.pop(0)
+        elif i[0]=="end":  print(o);break
+        else:  print(f"CMD:{cmd} UNDEFINED COMMAND");break
+        i.pop(0)
