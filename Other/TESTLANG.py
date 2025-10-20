b18480452868bb134fcd854a048c44ea9c789a72
Gaham <glebsemenov1mail.ru@gmail.com>
2025-10-20 12:33:49 +0300

    Update TESTLANG.py

--- Other/TESTLANG.py
+++ Other/TESTLANG.py
@@ -1,14 +1,13 @@
 i=input(">>").split()
 cmd=0;s=[];o=""
 while 1:
-        cmd+=1
+        cmd+=1;print(i[0:2],o)
         if i[0]=="out":
-            if i[1]=="char":  o+=chr(int(i.pop(1)))+" ";i.pop(0)
-            else:  o+=i.pop(1);i.pop(0)
+            if i[1]=="char":  o+=chr(int(i.pop(1)))+" ";i.pop(0);i.pop(0)
+            elif i[1]=="stack":  o+=str(s.pop())+" ";i.pop(0)
         elif i[0]=="note":  i.pop(0)
         elif i[0]=="push":
             if i[1]=="in":  s.append(input("input\n"));i.pop(0)
-            elif i[1]=="out":  o+=str(s.pop())+" ";i.pop(0)
             elif i[1]=="top":  s.append(s[-1]);i.pop(0)
             else:  s.append(i[1]);i.pop(0)
         elif i[0]=="op":
