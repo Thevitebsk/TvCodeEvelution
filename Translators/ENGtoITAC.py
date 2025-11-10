8093cb69c60c82b537147b40423d8314fe521e0f
Gaham <glebsemenov1mail.ru@gmail.com>
2025-11-11 00:28:46 +0300

    Create Translators/ENGtoITAC.py

---
+++ Translators/ENGtoITAC.py
@@ -0,0 +1,24 @@
+A=input("English: ").lower().split(" ");B=0;C=[]
+while B!=len(A):
+  if A[B]=="and":C.append("i")
+  elif A[B]=="hate":C.append("hrrr")
+  elif A[B]in["no","not","don't","doesn't"]:C.append("hr")
+  elif A[B]=="maybe":C.append("wy")
+  elif A[B]in ["greet","hi","hello"]:C.append("hi")
+  elif A[B]=="say":C.append("tag")
+  elif A[B]=="to":
+  	 	if C:C[-1]+="o"
+  	 	else:()
+  elif A[B]=="on":
+  		 if C:C[-1]+="a"
+  		 else:()
+  elif A[B]=="active":C.append("systipu")
+  elif A[B]in["me","i"]:C.append("esel")
+  elif A[B]=="you":C.append("hr esel")
+  elif A[B]in["he","him"]:C.append("msel")
+  elif A[B]in["she","her"]:C.append("rsel")
+  elif A[B]in["it","this","they","them"]:C.append("isel")
+  elif A[B][0:3]=="pay"and A[B+1]=="attention":C.append("scaselet");B+=1
+  elif A[B]in["here","there"]:C[-1]+="ui";C.append("place")
+  B+=1
+print("   ITAC: "+" ".join(C)if C else None)
