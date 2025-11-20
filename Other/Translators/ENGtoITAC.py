7c0fc13bbc672a3b9febb487dc1c8c3f1861a485
Gaham <glebsemenov1mail.ru@gmail.com>
2025-11-20 21:50:11 +0300

    Update ENGtoITAC.py

--- Other/Translators/ENGtoITAC.py
+++ Other/Translators/ENGtoITAC.py
@@ -6,12 +6,8 @@ while B!=len(A):
   elif A[B]=="maybe":C.append("wy")
   elif A[B]in ["greet","hi","hello"]:C.append("hi")
   elif A[B]=="say":C.append("tag")
-  elif A[B]=="to":
-    if C:C[-1]+="o"
-    else:()
-  elif A[B]=="on":
-    if C:C[-1]+="a"
-    else:()
+  elif A[B]=="to":C[-1]+="o"if C else""
+  elif A[B]=="on":C[-1]+="a"if C else""
   elif A[B]=="active":C.append("systipu")
   elif A[B]in["me","i"]:C.append("esel")
   elif A[B]=="you":C.append("hr esel")
@@ -19,12 +15,10 @@ while B!=len(A):
   elif A[B]in["she","her"]:C.append("rsel")
   elif A[B]in["it","this","they","them"]:C.append("isel")
   elif A[B][0:3]=="pay"and A[B+1]=="attention":C.append("scaselet");B+=1
-  elif A[B]in["here","there"]:
-    if C:C[-1]+="ui";C.append("wy")
-    else:()
+  elif A[B]in["here","there"]:C[-1]+="ui"if C else"";C.append("wy")
   elif A[B]=="place":C.append("iselui")
   elif A[B]=="is":
     if A[B+1]!="by":C[-1]+="h"
-    else:C[-1]+="for";B+=1
+    else:C[-1]+="po";B+=1
   B+=1
 print("   ITAC: "+" ".join(C)if C else None)
