9baa9f458a31db7ab26b257215e8f951aa46b47f
Gaham <glebsemenov1mail.ru@gmail.com>
2025-11-26 00:06:27 +0300

    Update oldhum.py

--- Other/oldhum.py
+++ Other/oldhum.py
@@ -1,38 +1,18 @@
-"""This esolang was made by Gaham (User:Ractangle) during his IT lesson becuase he didn't want to make a very complicated project (A.K.A. Making a sims game in python)
-`...` pushes every symbol surrounded by ` to string stack
-> move all the elements on the string stack to the stack
-@ prints the top value on the stack
-# starts program. stops program if encounterd again.
-! Pops the top value on the (string) stack
-R Flips the (string) stack
-M Changes the stack pointers position
-|...| Comment
-_ input"""
-m="";s=[];ss=[];os=[];sp=0;inp=[]
+"""This esolang was made by me during my IT lesson becuase i didn't want to make a \"very complicated project\""""
 def code(i,u):
-    global m,sp
+    s1=[];s2=[];os=[];sp=p=0;inp=[];ss=[s1,s2]
     for t in u:
         inp.append(t)
-    for c in i:
-        if m=="e":break
-        elif c=="`" and m=="c":m="s"
-        elif c=="`" and m=="s":m="c"
-        elif m=="s":ss.append(c)
-        elif c=="@" and m=="c":s.reverse();os.append(s[0]);s.reverse();s.pop()
-        elif c==">" and m=="c":
-            while len(ss)>1:a=str(ss[0])+str(ss[1]);ss.pop(0);ss.pop(0);ss.reverse();ss.append(a);ss.reverse()
-            while len(ss)>0:s.append(ss[0]);ss.pop(0)
-        elif c=="#":m="c"
-        elif c=="!" and sp==0:ss.pop()
-        elif c=="!" and sp==1:s.pop()
-        elif c=="R" and sp==0:ss.reverse()
-        elif c=="R" and sp==1:s.reverse()
-        elif c=="M" and sp==0:sp=1
-        elif c=="M" and sp==1:sp=0
-        elif c=="|":m=="no"
-        elif c=="|" and m=="no":m==c
-        elif c=="_" and m=="c":s.append(inp[0]);inp.pop(0)
+    while p!=len(i):
+        if i[p]=="`":s2.append(i[p+1:i.index("`",p+1)]);p=i.index("`",p+1)
+        elif i[p]=="@":os.append(s1.pop())
+        elif i[p]==">":s1.append("".join(s2))
+        elif i[p]=="!":ss[sp].pop()
+        elif i[p]=="R":ss[sp].reverse()
+        elif i[p]=="M":sp=int(not sp)
+        elif i[p]=="|":p=i.index("|",p)
+        elif i[p]=="_":s1.append(inp.pop(0))
         else:print("undefined command");break
-    print(f"code:\n{i}\nstring stack:\n{ss}\nstack:\n{s}\ninput:\n{u}\noutput:")
-    while len(os)>0:print(os[0]);os.pop(0)
-code("#`Hello world`>@","input")
+        p+=1
+    print(f"code:   {i}\ninput:  {u}\noutput: {"".join(os[0])}")
+code("`Hello world`>@","input")
