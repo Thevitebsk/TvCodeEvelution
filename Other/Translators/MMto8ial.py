1cf42ee86c2625fb5fa9c4746f410404a75444d3
Gaham <glebsemenov1mail.ru@gmail.com>
2025-11-27 22:54:17 +0300

    Create MMto8ial.py

---
+++ Other/Translators/MMto8ial.py
@@ -0,0 +1,18 @@
+#shut up it counts as a translator to me
+
+mm=input("MM:").split(" ");p=x=0;r={}
+def MM(x:int)->str:
+  a=["0","1","2","3","4","5","6","7","8","9",
+     "A","B","C","D","E","F"]
+  return (str(a[x%(F:=16)])+ \
+  str(a[x//F%F])+str(a[x//F//F%F])+ \
+  str(a[x//F//F//F%F])+str(a[x//F//F//F//F%F]))[::-1]
+while len(mm)!=p:
+  print(f";{MM(x)}",end=" ")
+  if mm[p][0]=="0":   print(f"INC ${int(mm[p][1:])}",end=" ");x+=1
+  elif mm[p][0]=="1":
+    try:    print(f"JIR {MM(r[int(mm[p+1])])} ${int(mm[p][1:])} 0 DEC ${int(mm[p][1:])}",end=" ")
+    except: r[int(mm[p+1])]=0;print(f"JIR {MM(r[int(mm[p+1])])} ${int(mm[p+1])} 0 DEC ${int(mm[p][1:])}",end=" ")
+    p+=1
+  p+=1
+print("END")
