c6cb8345e5f8560d3618484d0f40f071aedc46b9
Gaham <glebsemenov1mail.ru@gmail.com>
2025-12-20 23:34:09 +0300

    Update 8ial.py

--- Other/8ial.py
+++ Other/8ial.py
@@ -1,24 +1,27 @@
 import sys;a={};b=[];c=d=0
 for e in range(1,17):a[e]=0
 try:f=open(sys.argv[1]).readlines()
-except(FileNotFoundError,IndexError):print("not file was chosen")
+except(FileNotFoundError,IndexError):print("no file was chosen")
 for g in f:[b.append(i)for i in g.split()]
+def h(x:str):
+    try:int(x);return True
+    except:return False
 while b[c]!="END"and d<4096:
-    if b[c]=="INC":a[int(b[c+1][1])]=(a[int(b[c+1][1])]+1)%256;c+=1
-    elif b[c]=="OUT":print(a[int(b[c+1][1])],end=" ")
+    if b[c]=="INC":a[int(b[c+1][1:])]=(a[int(b[c+1][1:])]+1)%256;c+=1
+    elif b[c]=="OUT":print(a[int(b[c+1][1:])],end=" ")
     elif b[c]=="JMP":
         i=b[c+1];c=0;j=b[0:c].count("i")
         while c<len(b):
             if b[c]==";"+i and not j:break
             c+=1
-    elif b[c]=="PUT":a[int(b[c+1][1])]=int(input("\n"))%256;c+=1
+    elif b[c]=="PUT":a[int(b[c+1][1:])]=int(input("\n"))%256;c+=1
     elif b[c]=="JIR":
-        if a[int(b[c+2][1])]==int(b[c+3])%256:
+        if a[int(b[c+2][1:])]==(int(b[c+3])if h(b[c+3])else a[int(b[c+3][1:])])%256:
             i=b[c+1];c=0;j=b[0:c].count("i")
             while c<len(b):
                 if b[c]==";"+i and not j:break
                 c+=1
         else:c+=3
-    elif b[c]=="DEC":a[int(b[c+1][1])]=(a[int(b[c+1][1])]-1)%256;c+=1
+    elif b[c]=="DEC":a[int(b[c+1][1:])]=(a[int(b[c+1][1:])]-1)%256;c+=1
     c+=1;d+=1
 print(f"\nOP:{len(b):>18}\nACTUAL BYTESIZE:   {len(open(sys.argv[1]).read())}")
