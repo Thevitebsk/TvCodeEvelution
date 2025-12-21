aa65e2b46dc02b95dcdfe97fd01dfd363c285af7
Gaham <glebsemenov1mail.ru@gmail.com>
2025-12-21 20:48:58 +0300

    finaly it doesn't add a newline everytime

--- Other/8ial.py
+++ Other/8ial.py
@@ -1,4 +1,4 @@
-import sys;a={};b=[];c=d=0
+import sys;a={};b=[];c=d=k=0
 for e in range(1,17):a[e]=0
 try:f=open(sys.argv[1]).readlines()
 except(FileNotFoundError,IndexError):print("no file was chosen");exit()
@@ -8,14 +8,14 @@ def h(x:str):
     except:return False
 while b[c]!="END"and d<4096:
     if b[c]=="INC":a[int(b[c+1][1:])]=(a[int(b[c+1][1:])]+1)%256;c+=1
-    elif b[c]=="OUT":print(a[int(b[c+1][1:])],end=" ")
+    elif b[c]=="OUT":print(a[int(b[c+1][1:])],end=" ");c+=1;k=1
     elif b[c]=="JMP":
         i=b[c+1];c=0;j=b[0:c].count("i")
         while c!=len(b):
             if b[c]==";"+i and not j:break
             c+=1
         else:print(f"no label \"{i}\" found",c);break
-    elif b[c]=="PUT":a[int(b[c+1][1:])]=int(input("\n"))%256;c+=1
+    elif b[c]=="PUT":a[int(b[c+1][1:])]=int(input("\n"*k))%256;c+=1;k=0
     elif b[c]=="JIR":
         if a[int(b[c+2][1:])]==(int(b[c+3])if h(b[c+3])else a[int(b[c+3][1:])])%256:
             i=b[c+1];c=0;j=b[0:c].count("i")
