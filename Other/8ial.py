97301176e2cff014926d48562d071790455a3093
Gaham <glebsemenov1mail.ru@gmail.com>
2025-12-21 18:35:23 +0300

    Update 8ial.py

--- Other/8ial.py
+++ Other/8ial.py
@@ -28,6 +28,6 @@ while b[c]!="END"and d<4096:
     elif b[c]=="DEC":a[int(b[c+1][1:])]=(a[int(b[c+1][1:])]-1)%256;c+=1
     elif b[c][0]in[";","$"]:...
     else:print(f"undefined instruction {b[:c]}");break
-    print(b[:c])
+    print(b[c])
     c+=1;d+=1
 print(f"\nOP:{len(b):>18}\nACTUAL BYTESIZE:   {len(open(sys.argv[1]).read())}")
