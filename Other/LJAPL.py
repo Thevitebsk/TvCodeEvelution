6cf5947e9a41d663fb56f089e824ac3af260c65a
Gaham <glebsemenov1mail.ru@gmail.com>
2025-12-29 00:31:21 +0300

    Update LJAPL.py

--- Other/LJAPL.py
+++ Other/LJAPL.py
@@ -4,20 +4,17 @@ L       J   A A   P  P L
 L       J  AAAAA  PPPP L
 LLLL JJJ  A     A P    LLLL
 Literally Just A Programing Language""")
-n="\n"
-while True:
-  ce=f"0123456789{n} abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+,-./:;<=>?@[\]`|~"
-  s=[];nc=0;b=[];e=0;num=[]
+while 1:
+  ce="0123456789\n abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+,-./:;<=>?@[\\]`|~"
+  s=[];nc=e=0;b=[]
   out=[];inp=input(">>")
   if inp=="exit":break
-  while len(inp)>nc:
+  while len(inp)!=nc:
     if inp[nc]=="+":s.append(int(s.pop(0))+int(s.pop(0)))
     elif inp[nc]in ce[0:10]:s.append(int(inp[nc]))
     elif inp[nc]=="a":s.append(10)
-    elif inp[nc]=="|":
-      while len(out)>0:print(out.pop(0),end="")
-      print();break
-    elif inp[nc]=="n":out.append(int(s.pop(0)))
+    elif inp[nc]=="|":print("".join(out));break
+    elif inp[nc]=="n":out.append(str(int(s.pop(0))))
     elif inp[nc]=="c":out.append(ce[int(s.pop(0))])
     elif inp[nc]=="*":s.append(int(s.pop(0))*int(s.pop(0)))
     elif inp[nc]=="=":s.append(s[-1])
