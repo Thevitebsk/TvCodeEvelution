250f789f834f6a075774eb041038b5f648e9cd50
Gaham <glebsemenov1mail.ru@gmail.com>
2026-01-10 22:50:43 +0300

    Update array.py

--- Other/array.py
+++ Other/array.py
@@ -6,16 +6,16 @@ while p<len(c):
  if c[p]=="[" and m==0:m=1
  elif m==1:
   if c[p]=="," and c[p-1]!="\\":
-   s.reverse()
-   while len(s)>1:s.append(s.pop()+s.pop())
-   ar.append(s[0]);s.pop()
+   while len(s)>1:s.append(s.pop(0)+s.pop(0))
+   ar.append(s.pop())
   s.append(c[p])
   if c[p]=="]":
    m=0;s.pop()
    if s[0]==",":s.pop(0)
    s.reverse()
    while len(s)>1:s.append(s.pop()+s.pop())
-   ar.append(s.pop(0).replace("\\",""))
+   print(s)
+   ar.append(s.pop().replace("\\",""))
  elif c[p]=="∃": p=c.index("∄",p)
  elif c[p]=="□": ar.append(input())
  p+=1
