496c7995d8a55792c462d19a63c82de368270cfa
Gaham <glebsemenov1mail.ru@gmail.com>
2026-02-28 12:12:09 +0300

    decided to finish it up a bit

--- 4ME/gammascriptalpha.py
+++ 4ME/gammascriptalpha.py
@@ -1,27 +1,34 @@
-#U8LL.py + MPUG.py
 #this version of gammascript is really incomplete and will probably never will be complete
-import re
-class Lexer(object):
-    def __init__(self, source_code):
-        self.source_code=source_code
-    def tokenize(self):
-        token=[]
-        source_code=self.split()
-        source_index=0
-        while source_index<len(source_code):
-            word = source_code[source_index]
-            if word == "write":
-                token.append(["WRITE", word])
-            elif re.match('[a-z]', word) or re.match('[A-Z]', word):
-                token.append(['IDENTIFIER', word])
-            elif re.match('[0-9]', word):
-                token.append(['IDENTEGER', word])
-            source_index +=1
-        print(token)
-        return token
-def main():
-    content = ""
-    with open('test.gr') as file:
-        content = file.read()
-    lex=Lexer(content).tokenize()
-main()
+class gs(object):
+    def __init__(self,code):
+        self.code=code
+        er=self.run(self.tokenize(code))
+        try:int(er);print("-"*32,"PROGRAM ENDED AND RETURNED AN ERROR")
+        except (ValueError,TypeError):0
+        else:0
+    def tokenize(self,code):
+        tokes=[]
+        pointer=0
+        code=code.replace("{"," { ").replace("}"," } ").split()
+        while pointer!=len(code):
+            if code[pointer]=="write":
+                tokes.append(["print",code[pointer]])
+            elif code[pointer]in"{}":
+                tokes.append(["bracket",code[pointer]])
+            else:
+                tokes.append(['id',code[pointer]])
+            pointer+=1
+        return tokes
+    def run(self,tokes:list):
+        pointer=0
+        while pointer!=len(tokes):
+            if tokes[pointer][0]=="print":
+                if tokes[pointer+1][0]!="bracket":
+                    print("Incorrect Syntax!");return 1
+                pointer+=2
+                while tokes[pointer][0]!="bracket":
+                    print(tokes[pointer][1],end=" ")
+                    pointer+=1
+            pointer+=1
+with open('C:\\Users\\Глеб\\AppData\\Roaming\\Code\\User\\History\\-517b750d\\0gmT.gr')as file:
+    gs(file.read())
