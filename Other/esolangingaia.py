8c1fc51ae4f3106ebda66151a160166d93feb686
Gaham <glebsemenov1mail.ru@gmail.com>
2026-03-01 18:45:16 +0300

    Update esolangingaia.py

--- Other/esolangingaia.py
+++ Other/esolangingaia.py
@@ -1,18 +1,20 @@
-#d9Dp.py
+#B8RB.py
 #tried to implement the esolang "1^"
 
-c=str("""32>|""")
+com='><=|n'
+num=list(map(str,range(10)))
+c="""32>|"""
 p=0;s=[]
 print("1^")
 while True:
-  if c[p]in list(map(str,range(10))):s.append(int(c[p]))
-  elif c[p]==">":
+  if c[p]in num:s.append(int(c[p]))
+  elif c[p]in com[0]:
     if s.pop()>s.pop():pass
     else:p+=1
-  elif c[p]=="|":break
-  elif c[p]=="n":
+  elif c[p]in com[3]:break
+  elif c[p]in com[4]:
     while c[p]!="\n":p+=1
-  elif c[p]=="\n"or len(c)<p:
+  elif c[p]=="\n"or len(c)<=p:
     print(c[p])
     while c[p]!="\n"or p!=0:p-=1
   print(c[p]);p+=1
