c76070ec5615c600c1535d0b9ba97623bfc721af
Gleb <g89784734@gmail.com>
2026-04-19 21:27:40 +0300

    8ial update

--- Other/8ial.py
+++ Other/8ial.py
@@ -1,32 +1,43 @@
-import sys;regs={};ops=[];p=d=ne=0
-for _ in range(1,17):regs[_]=0
+import sys;regs={};ops=[];p=ne=0
+for _ in range(0,16):regs[_]=0
 try:f=open(sys.argv[1]).readlines()
-except(FileNotFoundError,IndexError):print("no file was chosen");exit()
-for _ in f:[ops.append(_2)for _2 in _.split()]
+except(FileNotFoundError,IndexError):
+    print("no file is used or it doesn't exist")
+    exit()
+for _ in f:
+    for _2 in _.split(): ops.append(_2)
 def is_int(x:str):
-    try:int(x);return True
-    except:return False
-def eval(x):return int(x,16)%16 if is_int(x)else int(regs[int(x[1:])]%256)
-while ops[p]!="END"and d<4096:
-    if ops[p]=="INC":regs[int(ops[p+1][1:])]=(regs[int(ops[p+1][1:])]+1)%256;p+=1
-    elif ops[p]=="OUT":print(regs[int(ops[p+1][1:])],end=" ");p+=1;ne=1
+    try: int("0x"+x,16); return True
+    except: return False
+def eval(x):
+    if is_int(x):return int(x,16)%16
+    else: return int(regs[int("0x"+x[1:],16)]%256)
+while ops[p]!="END":
+    if ops[p]=="INC":\
+     regs[int("0x"+ops[p+1][1:],16)]=(regs[int("0x"+ops[p+1][1:],16)]+1)%256;p+=1
+    elif ops[p]=="OUT":
+        print(regs[int("0x"+ops[p+1][1:],16)],end=" ");p+=1;ne=1
     elif ops[p]=="JMP":
-        i=ops[p+1];p=0;j=ops[0:p].count("i")
+        i=ops[p+1];j=ops.count(f";{i}");p=0
         while p!=len(ops):
-            if ops[p]==";"+i and not j:break
+            if ops[p]==f";{i}":j-=1
+            if j==0:break
             p+=1
-        print(f"no label \"{i}\" found");break
-    elif ops[p]=="PUT":regs[int(ops[p+1][1:])]=int(input("\n"*ne))%256;p+=1;ne=0
-    elif ops[p]=="JIR":
+        if p==len(ops):print(f"no label \"{i}\" found");break
+    elif ops[p]=="PUT":\
+     regs[int("0x"+ops[p+1][1:],16)]=int("0x"+input("\n"*ne),16)%256;p+=1;ne=0
+    elif ops[p]=="CMJ":
         if eval(ops[p+2])==eval(ops[p+3]):
-            i=ops[p+1];p=0;j=ops[0:p].count("i")
+            i=ops[p+1];j=ops.count(f";{i}");p=0
             while p!=len(ops):
-                if ops[p]==";"+i and not j:break
+                if ops[p]==f";{i}":j-=1
+                if j==0:break
                 p+=1
-            print(f"no label \"{i}\" found");break
+            if p==len(ops): print(f"no label \"{i}\" found");break
         else:p+=3
-    elif ops[p]=="DEC":regs[int(ops[p+1][1:])]=(regs[int(ops[p+1][1:])]-1)%256;p+=1
-    elif ops[p][0]in[";","$"]:...
-    else:print(f"undefined instruction \"{ops[p]}\"",p);break
-    p+=1;d+=1
+    elif ops[p]=="DEC":\
+     regs[int("0x"+ops[p+1][1:],16)]=(regs[int("0x"+ops[p+1][1:],16)]-1)%256;p+=1
+    elif ops[p][0]in[";","$"]: ...
+    else: print(f"undefined instruction \"{ops[p]}\"",p);break
+    p+=1
 print(f"\nOP:{len(ops):>18}\nACTUAL BYTESIZE:   {len(open(sys.argv[1]).read())}")
